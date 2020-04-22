30f0326efa0160e2872d7e969f3fab0076322e13
SKP <skp.frl@gmail.com>
2020-04-22 16:09:46 +0530

    :bug: Fix server not serving files properly

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 /* eslint-disable */
-const express = require('express');
-const connectHistory = require('connect-history-api-fallback');
+const express = require('express')
+const path = require('path')
+const history = require('connect-history-api-fallback')
 
-const app = express();
+const app = express()
+const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.use(
-  connectHistory()
-);
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+  res.render(path.join(__dirname + '/index.html'))
+})
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server listening on port %d!', this.address().port)
+})
