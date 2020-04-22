6ded9227627e8901e2d1d5561804e9bffa125d82
SKP <skp.frl@gmail.com>
2020-04-22 14:26:56 +0530

    :iphone: Make app responsive

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,15 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-@import './utils/colors';
+@import "./utils/colors";
+@import "./utils/media";
 
 #app {
   height: 100vh;
@@ -28,14 +29,34 @@ export default class App extends Vue {}
   -webkit-tap-highlight-color: inherit;
 }
 
+::selection {
+  background: $primary;
+}
+
 html,
 body {
   font-size: 10px;
-  font-family: 'Montserrat', sans-serif;
+  font-family: "Montserrat", sans-serif;
   box-sizing: border-box;
   margin: 0;
   background-color: $background;
   color: $white;
   -webkit-tap-highlight-color: transparent;
+
+  @include tablet {
+    font-size: 11px;
+  }
+
+  @include laptopL {
+    font-size: 13px;
+  }
+
+  @include desktop {
+    font-size: 16px;
+  }
+
+  @include desktopL {
+    font-size: 20px;
+  }
 }
 </style>
