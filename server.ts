247b8eccd3490afee3f23fba050e34eb8fbcc5bb
SKP <skp.frl@gmail.com>
2020-04-22 13:48:39 +0530

    :sparkles: Configure server

---
+++ server.ts
@@ -0,0 +1,10 @@
+const express = require('express');
+const connectHistory = require('connect-history-api-fallback');
+
+const app = express();
+
+app.use(
+  connectHistory({
+    index: '/dist/index.html',
+  })
+);
