0717b9eec5849db232d56247a47368a30071a37a
SKP <skp.frl@gmail.com>
2020-04-22 15:20:10 +0530

    :dizzy: Add animated transitions in TodosList

--- src/components/specific/TodosList.vue
+++ src/components/specific/TodosList.vue
@@ -1,24 +1,28 @@
 <template>
-  <ul class="todos-list" v-if="hasTodos">
-    <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
-  </ul>
-  <div v-else class="fallback">
-    <span>No tasks yet! Try adding one.</span>
-  </div>
+  <main class="todos-list">
+    <transition-group tag="ul" name="slide" class="list-container">
+      <todo-item v-for="todo in todos" :key="todo.id" :todo="todo" />
+    </transition-group>
+
+    <transition name="fade">
+      <span v-if="!hasTodos" class="fallback">No tasks yet! Try adding one.</span>
+      <span v-else-if="isFilteredTodosEmpty" class="fallback">No todos for this filter.</span>
+    </transition>
+  </main>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { getModule } from 'vuex-module-decorators';
-import TodoModule from '@/store/modules/TodoModule';
-import TodoItem from '@/components/specific/TodoItem.vue';
+import { Component, Vue } from "vue-property-decorator";
+import { getModule } from "vuex-module-decorators";
+import TodoModule from "@/store/modules/TodoModule";
+import TodoItem from "@/components/specific/TodoItem.vue";
 
 const todoMod = getModule(TodoModule);
 
 @Component({
   components: {
-    TodoItem,
-  },
+    TodoItem
+  }
 })
 export default class TodosList extends Vue {
   public get todos() {
@@ -26,26 +30,68 @@ export default class TodosList extends Vue {
   }
 
   public get hasTodos() {
-    return this.todos.length !== 0;
+    return todoMod.hasTodos;
+  }
+
+  public get isFilteredTodosEmpty() {
+    return this.todos.length === 0;
   }
 }
 </script>
 
 <style lang="scss" scoped>
+/* $bezier-curve: cubic-bezier(0.27, 1.72, 0.38, 0.92); */
+$bezier-curve: cubic-bezier(0.18, 0.31, 0, 1.51);
+
 .todos-list {
-  list-style: none;
   width: 100%;
   height: 100%;
+  display: block;
+  padding-bottom: 6rem;
   overflow-y: auto;
-  padding: 3rem 0rem;
-  margin: 0;
+  overflow-x: hidden;
+
+  & > .list-container {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  & > .fallback {
+    display: block;
+    font-size: 1.8rem;
+    margin: 1.5rem 2rem;
+  }
 }
 
-.fallback {
-  padding: 3rem 2rem;
+.slide-move {
+  transition: transform 1s $bezier-curve;
+}
 
-  & > span {
-    font-size: 1.8rem;
-  }
+.slide-enter-active {
+  transition: all 0.75s $bezier-curve;
+}
+
+.slide-leave-active {
+  position: absolute;
+  transition: all 0.6s ease-in-out;
+}
+
+.slide-enter {
+  transform: translateX(-110%);
+}
+
+.slide-leave-to {
+  transform: translateX(110%);
+}
+
+.fade-enter-active {
+  transition: all 0.25s ease-in-out;
+  transition-delay: 0.6s;
+}
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
