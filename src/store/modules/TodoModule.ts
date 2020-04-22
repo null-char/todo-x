dc01e8f073b15c6614e8b77b5e627a2d486e7730
SKP <skp.frl@gmail.com>
2020-04-22 15:20:38 +0530

    :sparkles: Add updateFilter mutation and todos getter

--- src/store/modules/TodoModule.ts
+++ src/store/modules/TodoModule.ts
@@ -5,30 +5,54 @@ import Todo from '@/types/Todo';
 //! The store must be passed into the Module decorator for dynamic modules to work
 @Module({ dynamic: true, store, namespaced: true, name: 'todo' })
 export default class TodoModule extends VuexModule {
-  todos: Todo[] = [];
+  private _todos: Todo[] = [];
+  public filter = '';
+
+  get todos() {
+    switch (this.filter) {
+      case 'all':
+        return this._todos;
+      case 'completed':
+        return this.completedTodos;
+      case 'incomplete':
+        return this.incompleteTodos;
+      default:
+        return this._todos;
+    }
+  }
 
   get completedTodos() {
-    return this.todos.filter((t) => t.isCompleted);
+    return this._todos.filter((t) => t.isCompleted);
   }
 
   // incomplete refers to todos that are still due to be done
   get incompleteTodos() {
-    return this.todos.filter((t) => !t.isCompleted);
+    return this._todos.filter((t) => !t.isCompleted);
+  }
+
+  // getter which computes whether or not there are any todos in the list
+  get hasTodos() {
+    return this._todos.length !== 0;
   }
 
   @Mutation
   addTodo(payload: Todo) {
-    this.todos.push(payload);
+    this._todos.push(payload);
   }
 
   @Mutation
   setTodoIsComplete(payload: { id: string; value: boolean }) {
-    const idx = this.todos.findIndex((t) => t.id === payload.id);
-    this.todos[idx].isCompleted = payload.value;
+    const idx = this._todos.findIndex((t) => t.id === payload.id);
+    this._todos[idx].isCompleted = payload.value;
   }
 
   @Mutation
   removeTodo(id: string) {
-    this.todos = this.todos.filter((t) => t.id !== id);
+    this._todos = this._todos.filter((t) => t.id !== id);
+  }
+
+  @Mutation
+  updateFilter(newFilter: string) {
+    this.filter = newFilter;
   }
 }
